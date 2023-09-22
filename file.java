c0aade11fde8ffbe410a433a24399ce845e8ca6e
muggullas <60910214+muggullas@users.noreply.github.com>
2023-09-22 08:44:55 +0530

    Update file.java

--- file.java
+++ file.java
@@ -1,33 +1,113 @@
-module "ec2_instance" {
-  source  = "./modules/aws-ec2"
+data "aws_partition" "current" {}
 
-  #for_each = toset(["one", "two", "three"])
-  name = "dat-${each.key}"
-  for_each = {
-    one   = {
-      instance_type        = "t3.micro"
-      subnet_id            = "subnet-2529cb04"
-      vpc_security_group_ids = ["sg-0684776069a694f30"]
-      ebs_block_device     = [
-        { device_name = "/dev/sdb", volume_type = "gp2", volume_size = 35, delete_on_termination = true }
-      ]
-    }
-    two   = {
-      instance_type        = "t2.micro"
-      subnet_id            = "subnet-2529cb04"
-      vpc_security_group_ids = ["sg-b1d6ca9a"]
-      ebs_block_device     = [
-        { device_name = "/dev/sdb", volume_type = "gp2", volume_size = 40, delete_on_termination = true }
-      ]
+locals {
+  create = var.create
+
+  is_t_instance_type = replace(var.instance_type, "/^t(2|3|3a|4g){1}\\..*$/", "1") == "1" ? true : false
+
+  ami = try(coalesce(var.ami, try(nonsensitive(data.aws_ssm_parameter.this[0].value), null)), null)
+}
+
+data "aws_ssm_parameter" "this" {
+  count = local.create && var.ami == null ? 1 : 0
+
+  name = var.ami_ssm_parameter
+}
+
+################################################################################
+# Instance
+################################################################################
+
+resource "aws_instance" "this" {
+  count = local.create ? 1 : 0
+
+  ami                  = local.ami
+  #windows_ami_ssm_parameter = var.windows_ami_ssm_parameter
+  instance_type        = var.instance_type
+  hibernation          = var.hibernation
+
+  user_data                   = var.user_data
+  user_data_base64            = var.user_data_base64
+  user_data_replace_on_change = var.user_data_replace_on_change
+
+  availability_zone      = var.availability_zone
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = var.vpc_security_group_ids
+
+  key_name             = var.key_name
+  #monitoring           = var.monitoring
+  get_password_data    = var.get_password_data
+  #iam_instance_profile = var.create_iam_instance_profile ? aws_iam_instance_profile.this[0].name : var.iam_instance_profile
+
+  associate_public_ip_address = var.associate_public_ip_address
+#  private_ip                  = var.private_ip
+#  secondary_private_ips       = var.secondary_private_ips
+#  ipv6_address_count          = var.ipv6_address_count
+#  ipv6_addresses              = var.ipv6_addresses
+
+  ebs_optimized = var.ebs_optimized
+
+  dynamic "root_block_device" {
+    for_each = var.root_block_device
+
+    content {
+      delete_on_termination = try(root_block_device.value.delete_on_termination, null)
+      encrypted             = try(root_block_device.value.encrypted, null)
+      iops                  = try(root_block_device.value.iops, null)
+      kms_key_id            = lookup(root_block_device.value, "kms_key_id", null)
+      volume_size           = try(root_block_device.value.volume_size, null)
+      volume_type           = try(root_block_device.value.volume_type, null)
+      throughput            = try(root_block_device.value.throughput, null)
+      tags                  = try(root_block_device.value.tags, null)
     }
   }
-tags =  {
+
+  dynamic "ebs_block_device" {
+    for_each = var.ebs_block_device
+
+    content {
+      delete_on_termination = try(ebs_block_device.value.delete_on_termination, null)
+      device_name           = ebs_block_device.value.device_name
+      encrypted             = try(ebs_block_device.value.encrypted, null)
+      iops                  = try(ebs_block_device.value.iops, null)
+      kms_key_id            = lookup(ebs_block_device.value, "kms_key_id", null)
+      snapshot_id           = lookup(ebs_block_device.value, "snapshot_id", null)
+      volume_size           = try(ebs_block_device.value.volume_size, null)
+      volume_type           = try(ebs_block_device.value.volume_type, null)
+      throughput            = try(ebs_block_device.value.throughput, null)
+      tags                  = try(ebs_block_device.value.tags, null)
+    }
+  }
+
+  dynamic "ephemeral_block_device" {
+    for_each = var.ephemeral_block_device
+
+    content {
+      device_name  = ephemeral_block_device.value.device_name
+      no_device    = try(ephemeral_block_device.value.no_device, null)
+      virtual_name = try(ephemeral_block_device.value.virtual_name, null)
+    }
+  }
+
+
+  dynamic "launch_template" {
+    for_each = length(var.launch_template) > 0 ? [var.launch_template] : []
+
+    content {
+      id      = lookup(var.launch_template, "id", null)
+      name    = lookup(var.launch_template, "name", null)
+      version = lookup(var.launch_template, "version", null)
+    }
+  }
+
+
+  host_id                              = var.host_id
+
+tags        = merge({ "Name" = var.name }, var.instance_tags, var.tags)
+
+# For Volume
+volume_tags =  {
   "Tag2" = "Tag2_Value_Here",
   "Tag3" = "Tag3_Value_Here"
 }
-
-  instance_type = each.value.instance_type
-  subnet_id = each.value.subnet_id
-  vpc_security_group_ids = each.value.vpc_security_group_ids
-  ebs_block_device = each.value.ebs_block_device
 }
