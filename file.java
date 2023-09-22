c843bbd5742b668bd54bec6d62e420e9c1a05b69
muggullas <60910214+muggullas@users.noreply.github.com>
2023-09-22 09:00:26 +0530

    Update file.java

--- file.java
+++ file.java
@@ -1,147 +1,30 @@
-variable "create" {
-  description = "Whether to create an instance"
-  type        = bool
-  default     = true
+# Set whether to create an instance
+create = true
+
+# Specify the name for the EC2 instance
+name = "my-instance-name"
+
+# Specify the SSM parameter name for the AMI ID
+ami_ssm_parameter = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+
+# Specify whether to associate a public IP address with the instance in a VPC
+associate_public_ip_address = true
+
+# Specify the availability zone for the instance (uncomment and specify a value if needed)
+availability_zone = "us-west-2a"
+
+# Specify whether the launched EC2 instance will be EBS-optimized
+ebs_optimized = true
+
+# Specify whether Nitro Enclaves will be enabled on the instance
+enclave_options_enabled = true
+
+instance_tags = {
+  Environment = "Development"
+  Owner      = "John Doe"
 }
 
-variable "name" {
-  description = "Name to be used on EC2 instance created"
-  type        = string
-  default     = ""
-}
-
-variable "ami_ssm_parameter" {
-  description = "SSM parameter name for the AMI ID. For Amazon Linux AMI SSM parameters see [reference](https://docs.aws.amazon.com/systems-manager/latest/userguide/parameter-store-public-parameters-ami.html)"
-  type        =  string
-  default     = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
-}
-
-variable "ami" {
-  description = "ID of AMI to use for the instance"
-  type        = string
-  default     = null
-}
-
-variable "associate_public_ip_address" {
-  description = "Whether to associate a public IP address with an instance in a VPC"
-  type        = bool
-  default     = null
-}
-
-variable "availability_zone" {
-  description = "AZ to start the instance in"
-  type        = string
-  default     = null
-}
-
-variable "ebs_block_device" {
-  description = "Additional EBS block devices to attach to the instance"
-  type        = list(any)
-  default     = []
-}
-
-variable "ebs_optimized" {
-  description = "If true, the launched EC2 instance will be EBS-optimized"
-  type        = bool
-  default     = null
-}
-
-variable "enclave_options_enabled" {
-  description = "Whether Nitro Enclaves will be enabled on the instance. Defaults to `false`"
-  type        = bool
-  default     = null
-}
-
-variable "ephemeral_block_device" {
-  description = "Customize Ephemeral (also known as Instance Store) volumes on the instance"
-  type        = list(map(string))
-  default     = []
-}
-
-variable "get_password_data" {
-  description = "If true, wait for password data to become available and retrieve it"
-  type        = bool
-  default     = null
-}
-
-variable "hibernation" {
-  description = "If true, the launched EC2 instance will support hibernation"
-  type        = bool
-  default     = null
-}
-
-variable "host_id" {
-  description = "ID of a dedicated host that the instance will be assigned to. Use when an instance is to be launched on a specific dedicated host"
-  type        = string
-  default     = null
-}
-
-variable "instance_initiated_shutdown_behavior" {
-  description = "Shutdown behavior for the instance. Amazon defaults this to stop for EBS-backed instances and terminate for instance-store instances. Cannot be set on instance-store instance" # https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/terminating-instances.html#Using_ChangingInstanceInitiatedShutdownBehavior
-  type        = string
-  default     = null
-}
-
-variable "instance_type" {
-  description = "The type of instance to start"
-  type        = string
-  default     = "t3.micro"
-}
-
-variable "instance_tags" {
-  description = "Additional tags for the instance"
-  type        = map(string)
-  default     = {}
-}
-
-variable "key_name" {
-  description = "Key name of the Key Pair to use for the instance; which can be managed using the `aws_key_pair` resource"
-  type        = string
-  default     = null
-}
-variable "root_block_device" {
-  description = "Customize details about the root block device of the instance. See Block Devices below for details"
-  type        = list(any)
-  default     = []
-}
-
-variable "secondary_private_ips" {
-  description = "A list of secondary private IPv4 addresses to assign to the instance's primary network interface (eth0) in a VPC. Can only be assigned to the primary network interface (eth0) attached at instance creation, not a pre-existing network interface i.e. referenced in a `network_interface block`"
-  type        = list(string)
-  default     = null
-}
-
-variable "source_dest_check" {
-  description = "Controls if traffic is routed to the instance when the destination address does not match the instance. Used for NAT or VPNs"
-  type        = bool
-  default     = null
-}
-
-variable "subnet_id" {
-  description = "The VPC Subnet ID to launch in"
-  type        = string
-  default     = null
-}
-
-variable "tags" {
-  description = "A mapping of tags to assign to the resource"
-  type        = map(string)
-  default     = {}
-}
-variable "volume_tags" {
-  description = "A mapping of tags to assign to the devices created by the instance at launch time"
-  type        = map(string)
-  default     = {}
-}
-
-variable "enable_volume_tags" {
-  description = "Whether to enable volume tags (if enabled it conflicts with root_block_device tags)"
-  type        = bool
-  default     = true
-}
-
-variable "vpc_security_group_ids" {
-  description = "A list of security group IDs to associate with"
-  type        = list(string)
-  default     = null
+tags =  {
+  "environment" = "dev",
+  "project" = "dat"
 }
