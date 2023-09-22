e462ed264556081fd262536349ac09817654ee56
muggullas <60910214+muggullas@users.noreply.github.com>
2023-09-22 08:54:54 +0530

    Update file.java

--- file.java
+++ file.java
@@ -1,113 +1,147 @@
-data "aws_partition" "current" {}
-
-locals {
-  create = var.create
-
-  is_t_instance_type = replace(var.instance_type, "/^t(2|3|3a|4g){1}\\..*$/", "1") == "1" ? true : false
-
-  ami = try(coalesce(var.ami, try(nonsensitive(data.aws_ssm_parameter.this[0].value), null)), null)
+variable "create" {
+  description = "Whether to create an instance"
+  type        = bool
+  default     = true
 }
 
-data "aws_ssm_parameter" "this" {
-  count = local.create && var.ami == null ? 1 : 0
-
-  name = var.ami_ssm_parameter
+variable "name" {
+  description = "Name to be used on EC2 instance created"
+  type        = string
+  default     = ""
 }
 
-################################################################################
-# Instance
-################################################################################
-
-resource "aws_instance" "this" {
-  count = local.create ? 1 : 0
-
-  ami                  = local.ami
-  #windows_ami_ssm_parameter = var.windows_ami_ssm_parameter
-  instance_type        = var.instance_type
-  hibernation          = var.hibernation
-
-  user_data                   = var.user_data
-  user_data_base64            = var.user_data_base64
-  user_data_replace_on_change = var.user_data_replace_on_change
-
-  availability_zone      = var.availability_zone
-  subnet_id              = var.subnet_id
-  vpc_security_group_ids = var.vpc_security_group_ids
-
-  key_name             = var.key_name
-  #monitoring           = var.monitoring
-  get_password_data    = var.get_password_data
-  #iam_instance_profile = var.create_iam_instance_profile ? aws_iam_instance_profile.this[0].name : var.iam_instance_profile
-
-  associate_public_ip_address = var.associate_public_ip_address
-#  private_ip                  = var.private_ip
-#  secondary_private_ips       = var.secondary_private_ips
-#  ipv6_address_count          = var.ipv6_address_count
-#  ipv6_addresses              = var.ipv6_addresses
-
-  ebs_optimized = var.ebs_optimized
-
-  dynamic "root_block_device" {
-    for_each = var.root_block_device
-
-    content {
-      delete_on_termination = try(root_block_device.value.delete_on_termination, null)
-      encrypted             = try(root_block_device.value.encrypted, null)
-      iops                  = try(root_block_device.value.iops, null)
-      kms_key_id            = lookup(root_block_device.value, "kms_key_id", null)
-      volume_size           = try(root_block_device.value.volume_size, null)
-      volume_type           = try(root_block_device.value.volume_type, null)
-      throughput            = try(root_block_device.value.throughput, null)
-      tags                  = try(root_block_device.value.tags, null)
-    }
-  }
-
-  dynamic "ebs_block_device" {
-    for_each = var.ebs_block_device
-
-    content {
-      delete_on_termination = try(ebs_block_device.value.delete_on_termination, null)
-      device_name           = ebs_block_device.value.device_name
-      encrypted             = try(ebs_block_device.value.encrypted, null)
-      iops                  = try(ebs_block_device.value.iops, null)
-      kms_key_id            = lookup(ebs_block_device.value, "kms_key_id", null)
-      snapshot_id           = lookup(ebs_block_device.value, "snapshot_id", null)
-      volume_size           = try(ebs_block_device.value.volume_size, null)
-      volume_type           = try(ebs_block_device.value.volume_type, null)
-      throughput            = try(ebs_block_device.value.throughput, null)
-      tags                  = try(ebs_block_device.value.tags, null)
-    }
-  }
-
-  dynamic "ephemeral_block_device" {
-    for_each = var.ephemeral_block_device
-
-    content {
-      device_name  = ephemeral_block_device.value.device_name
-      no_device    = try(ephemeral_block_device.value.no_device, null)
-      virtual_name = try(ephemeral_block_device.value.virtual_name, null)
-    }
-  }
-
-
-  dynamic "launch_template" {
-    for_each = length(var.launch_template) > 0 ? [var.launch_template] : []
-
-    content {
-      id      = lookup(var.launch_template, "id", null)
-      name    = lookup(var.launch_template, "name", null)
-      version = lookup(var.launch_template, "version", null)
-    }
-  }
-
-
-  host_id                              = var.host_id
-
-tags        = merge({ "Name" = var.name }, var.instance_tags, var.tags)
-
-# For Volume
-volume_tags =  {
-  "Tag2" = "Tag2_Value_Here",
-  "Tag3" = "Tag3_Value_Here"
+variable "ami_ssm_parameter" {
+  description = "SSM parameter name for the AMI ID. For Amazon Linux AMI SSM parameters see [reference](https://docs.aws.amazon.com/systems-manager/latest/userguide/parameter-store-public-parameters-ami.html)"
+  type        =  string
+  default     = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
 }
+
+variable "ami" {
+  description = "ID of AMI to use for the instance"
+  type        = string
+  default     = null
+}
+
+variable "associate_public_ip_address" {
+  description = "Whether to associate a public IP address with an instance in a VPC"
+  type        = bool
+  default     = null
+}
+
+variable "availability_zone" {
+  description = "AZ to start the instance in"
+  type        = string
+  default     = null
+}
+
+variable "ebs_block_device" {
+  description = "Additional EBS block devices to attach to the instance"
+  type        = list(any)
+  default     = []
+}
+
+variable "ebs_optimized" {
+  description = "If true, the launched EC2 instance will be EBS-optimized"
+  type        = bool
+  default     = null
+}
+
+variable "enclave_options_enabled" {
+  description = "Whether Nitro Enclaves will be enabled on the instance. Defaults to `false`"
+  type        = bool
+  default     = null
+}
+
+variable "ephemeral_block_device" {
+  description = "Customize Ephemeral (also known as Instance Store) volumes on the instance"
+  type        = list(map(string))
+  default     = []
+}
+
+variable "get_password_data" {
+  description = "If true, wait for password data to become available and retrieve it"
+  type        = bool
+  default     = null
+}
+
+variable "hibernation" {
+  description = "If true, the launched EC2 instance will support hibernation"
+  type        = bool
+  default     = null
+}
+
+variable "host_id" {
+  description = "ID of a dedicated host that the instance will be assigned to. Use when an instance is to be launched on a specific dedicated host"
+  type        = string
+  default     = null
+}
+
+variable "instance_initiated_shutdown_behavior" {
+  description = "Shutdown behavior for the instance. Amazon defaults this to stop for EBS-backed instances and terminate for instance-store instances. Cannot be set on instance-store instance" # https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/terminating-instances.html#Using_ChangingInstanceInitiatedShutdownBehavior
+  type        = string
+  default     = null
+}
+
+variable "instance_type" {
+  description = "The type of instance to start"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "instance_tags" {
+  description = "Additional tags for the instance"
+  type        = map(string)
+  default     = {}
+}
+
+variable "key_name" {
+  description = "Key name of the Key Pair to use for the instance; which can be managed using the `aws_key_pair` resource"
+  type        = string
+  default     = null
+}
+variable "root_block_device" {
+  description = "Customize details about the root block device of the instance. See Block Devices below for details"
+  type        = list(any)
+  default     = []
+}
+
+variable "secondary_private_ips" {
+  description = "A list of secondary private IPv4 addresses to assign to the instance's primary network interface (eth0) in a VPC. Can only be assigned to the primary network interface (eth0) attached at instance creation, not a pre-existing network interface i.e. referenced in a `network_interface block`"
+  type        = list(string)
+  default     = null
+}
+
+variable "source_dest_check" {
+  description = "Controls if traffic is routed to the instance when the destination address does not match the instance. Used for NAT or VPNs"
+  type        = bool
+  default     = null
+}
+
+variable "subnet_id" {
+  description = "The VPC Subnet ID to launch in"
+  type        = string
+  default     = null
+}
+
+variable "tags" {
+  description = "A mapping of tags to assign to the resource"
+  type        = map(string)
+  default     = {}
+}
+variable "volume_tags" {
+  description = "A mapping of tags to assign to the devices created by the instance at launch time"
+  type        = map(string)
+  default     = {}
+}
+
+variable "enable_volume_tags" {
+  description = "Whether to enable volume tags (if enabled it conflicts with root_block_device tags)"
+  type        = bool
+  default     = true
+}
+
+variable "vpc_security_group_ids" {
+  description = "A list of security group IDs to associate with"
+  type        = list(string)
+  default     = null
 }
