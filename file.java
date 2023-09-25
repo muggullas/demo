ae4a1eadaa0fa977d490c4028995716508f880ec
muggullas <60910214+muggullas@users.noreply.github.com>
2023-09-25 14:09:58 +0530

    Update file.java

--- file.java
+++ file.java
@@ -2,38 +2,35 @@ provider "aws" {
   region = "us-east-1" # Set your desired AWS region
 }
 
-# Define a map of S3 bucket configurations
-variable "buckets" {
-  type = map(object({
-    acl    = string # ACL for the bucket (e.g., "private", "public-read")
-    region = string # Region for the bucket
-    tags   = map(string) # Tags for the bucket
-  }))
-  default = {
-    example_bucket_1 = {
-      acl    = "private"
-      region = "us-east-1"
-      tags   = {
-        Name = "ExampleBucket1"
-      }
-    }
-    example_bucket_2 = {
-      acl    = "public-read"
-      region = "us-west-2"
-      tags   = {
-        Name = "ExampleBucket2"
-      }
-    }
-    # Add more bucket configurations as needed
-  }
-}
-
 resource "aws_s3_bucket" "buckets" {
   for_each = var.buckets
 
   bucket = each.key
   acl    = each.value.acl
   region = each.value.region
+
+  versioning {
+    enabled = each.value.versioning_enabled
+  }
+
+  dynamic "logging" {
+    for_each = can(lookup(each.value, "logging_target_bucket")) ? [1] : []
+    content {
+      target_bucket = each.value.logging_target_bucket
+      target_prefix = "logs/"
+    }
+  }
+
+  dynamic "cors_rule" {
+    for_each = length(each.value.cors_rules) > 0 ? [1] : []
+    content {
+      allowed_headers = each.value.cors_rules[0].allowed_headers
+      allowed_methods = each.value.cors_rules[0].allowed_methods
+      allowed_origins = each.value.cors_rules[0].allowed_origins
+      expose_headers  = each.value.cors_rules[0].expose_headers
+      max_age_seconds = each.value.cors_rules[0].max_age_seconds
+    }
+  }
 
   tags = each.value.tags
 }
