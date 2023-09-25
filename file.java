496bdac2fd3fde4050b7e4099118634638f1d042
muggullas <60910214+muggullas@users.noreply.github.com>
2023-09-25 13:51:32 +0530

    Update file.java

--- file.java
+++ file.java
@@ -1,1 +1,39 @@
-"/aws/service/ami-windows-latest/Windows_Server-2019-English-Full-Base"
+provider "aws" {
+  region = "us-east-1" # Set your desired AWS region
+}
+
+# Define a map of S3 bucket configurations
+variable "buckets" {
+  type = map(object({
+    acl    = string # ACL for the bucket (e.g., "private", "public-read")
+    region = string # Region for the bucket
+    tags   = map(string) # Tags for the bucket
+  }))
+  default = {
+    example_bucket_1 = {
+      acl    = "private"
+      region = "us-east-1"
+      tags   = {
+        Name = "ExampleBucket1"
+      }
+    }
+    example_bucket_2 = {
+      acl    = "public-read"
+      region = "us-west-2"
+      tags   = {
+        Name = "ExampleBucket2"
+      }
+    }
+    # Add more bucket configurations as needed
+  }
+}
+
+resource "aws_s3_bucket" "buckets" {
+  for_each = var.buckets
+
+  bucket = each.key
+  acl    = each.value.acl
+  region = each.value.region
+
+  tags = each.value.tags
+}
