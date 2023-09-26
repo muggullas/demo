ccee4f84a77c27b83aed76df5f45ab75939e7d70
muggullas <60910214+muggullas@users.noreply.github.com>
2023-09-26 08:59:10 +0530

    Update file.java

--- file.java
+++ file.java
@@ -1,29 +1,89 @@
-buckets = {
-  example_bucket_1 = {
-    acl    = "private"
-    region = "us-east-1"
-    tags = {
-      Name = "ExampleBucket1"
-    }
-    versioning_enabled = true
-    logging_target_bucket = "example-logs-bucket"
-    cors_rules = [
-      {
-        allowed_headers = ["Authorization"]
-        allowed_methods = ["GET", "PUT", "POST"]
-        allowed_origins = ["https://example.com"]
-        expose_headers  = ["ETag"]
-        max_age_seconds = 3600
-      },
-    ]
-  }
-  example_bucket_2 = {
-    acl    = "public-read"
-    region = "us-west-2"
-    tags = {
-      Name = "ExampleBucket2"
-    }
-    versioning_enabled = false
-  }
-  # Add more bucket configurations as needed
+main.tf:
+
+provider "aws" {
+  region = "us-east-1" # Set your desired AWS region
 }
+
+resource "aws_db_instance" "rds_instances" {
+  for_each = var.rds_instances
+
+  allocated_storage    = each.value.allocated_storage
+  storage_type         = each.value.storage_type
+  engine               = "postgres"
+  engine_version       = "12.6"
+  instance_class       = each.value.instance_class
+  name                 = each.key
+  username             = each.value.username
+  password             = each.value.password
+  skip_final_snapshot  = true
+  vpc_security_group_ids = [each.value.security_group_ids]
+  tags = each.value.tags
+}
+
+
+variables.tf:
+
+variable "rds_instances" {
+  type = map(object({
+    allocated_storage    = number
+    storage_type         = string
+    instance_class       = string
+    username             = string
+    password             = string
+    security_group_ids   = string
+    tags                 = map(string)
+  }))
+  default = {
+    example_instance_1 = {
+      allocated_storage    = 20
+      storage_type         = "gp2"
+      instance_class       = "db.t2.micro"
+      username             = "exampleuser1"
+      password             = "examplepassword1"
+      security_group_ids   = "sg-0123456789abcdef0"
+      tags = {
+        Name = "ExampleInstance1"
+      }
+    }
+    example_instance_2 = {
+      allocated_storage    = 50
+      storage_type         = "gp2"
+      instance_class       = "db.t2.small"
+      username             = "exampleuser2"
+      password             = "examplepassword2"
+      security_group_ids   = "sg-0123456789abcdef1"
+      tags = {
+        Name = "ExampleInstance2"
+      }
+    }
+    # Add more RDS instance configurations as needed
+  }
+}
+
+variables.tfvars:
+rds_instances = {
+  example_instance_1 = {
+    allocated_storage    = 20
+    storage_type         = "gp2"
+    instance_class       = "db.t2.micro"
+    username             = "exampleuser1"
+    password             = "examplepassword1"
+    security_group_ids   = "sg-0123456789abcdef0"
+    tags = {
+      Name = "ExampleInstance1"
+    }
+  }
+  example_instance_2 = {
+    allocated_storage    = 50
+    storage_type         = "gp2"
+    instance_class       = "db.t2.small"
+    username             = "exampleuser2"
+    password             = "examplepassword2"
+    security_group_ids   = "sg-0123456789abcdef1"
+    tags = {
+      Name = "ExampleInstance2"
+    }
+  }
+  # Add more RDS instance configurations as needed
+}
+
